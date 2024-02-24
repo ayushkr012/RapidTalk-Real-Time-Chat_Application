1c3020dd946a18f90f20ff8729bdb61aade6cbb7
ayushkr01-hub <ayushkumar29062003@gmail.com>
2024-02-25 03:28:48 +0530

    file updated

--- server/index.js
+++ server/index.js
@@ -1,35 +1,35 @@
-// Node server which will handle socket io connection
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const path = require("path");
 
-const io = require('socket.io')(8000, {
-  cors: {
-    origin: '*',
-  }
-});
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-const users={};
+const PORT = 8000;
 
-//
-io.on('connection', socket => {
+app.use(express.static(path.join(__dirname, "../public")));
 
-  // any user joined let other user connected to the server know! 
-  socket.on('new-user-joined', name => {
-    users[socket.id]=name;
-    socket.broadcast.emit('user-joined',name);
-    });
+const users = {};
 
-    //If someone sends a message broadcast, it to other people
-    socket.on('send', message =>{
-      console.log(message);
-        socket.broadcast.emit('recieve',{message:message, name:users[socket.id]})
-    });
-
-    // If someone leaves the chat let other know
-    socket.on('disconnect', message =>{
-      socket.broadcast.emit('left',users[socket.id])
-      delete users[socket.id];
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
   });
 
+  socket.on("send", (message) => {
+    console.log(message);
+    socket.broadcast.emit("recieve", { message, name: users[socket.id] });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
 });
 
-
-
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
